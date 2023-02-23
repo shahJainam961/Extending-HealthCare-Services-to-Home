c035464a6b0eb42ba27a7526dce22731583c332e
shahJainam961 <shahjainam961@gmail.com>
2023-02-23 22:41:54 +0530

    AFI-0004 Recpetionist able to create new empty health record of the patient

---
+++ src/main/java/com/team9/had/controller/ControllerForReceptionist.java
@@ -0,0 +1,31 @@
+package com.team9.had.controller;
+
+import com.team9.had.entity.HealthRecord;
+import com.team9.had.service.receptionist.ServiceForReceptionist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+
+@RestController
+@RequestMapping("/receptionist")
+public class ControllerForReceptionist {
+
+    @Autowired
+    private ServiceForReceptionist serviceForReceptionist;
+    @PostMapping("/createHealthRecord")
+    public ResponseEntity<Serializable> createHealthRecord(@RequestBody HealthRecord healthRecord){
+        Serializable obj = serviceForReceptionist.createHealthRecord(healthRecord);
+        if(obj != null){
+            return new ResponseEntity<>(obj, HttpStatusCode.valueOf(200));
+        }
+        else{
+            return new ResponseEntity<>(null, HttpStatusCode.valueOf(401));
+        }
+    }
+}
---
+++ src/main/java/com/team9/had/service/receptionist/ServiceForReceptionistImpl.java
@@ -0,0 +1,35 @@
+package com.team9.had.service.receptionist;
+
+import com.team9.had.Constant;
+import com.team9.had.entity.HealthRecord;
+import com.team9.had.repository.CitizenRepository;
+import com.team9.had.repository.DoctorRepository;
+import com.team9.had.repository.HealthRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Time;
+
+@Service
+public class ServiceForReceptionistImpl implements ServiceForReceptionist{
+
+    @Autowired
+    private HealthRecordRepository healthRecordRepository;
+
+    @Override
+    public Serializable createHealthRecord(HealthRecord healthRecord) {
+        try{
+            healthRecord.setCreationDate(new Date(System.currentTimeMillis()));
+            healthRecord.setCreationTime(new Time(System.currentTimeMillis()));
+            healthRecord.setStatus(Constant.HEALTH_RECORD_NOT_ASSESSED);
+            healthRecordRepository.save(healthRecord);
+            return true;
+        }
+        catch(Exception e){
+            System.out.println("e = " + e);
+            return null;
+        }
+    }
+}
