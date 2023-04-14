721f19fde18776e0ec84f6ad0f8b73eeb9fb9764
tanishkasinghal <=>
2023-04-14 16:01:32 +0530

    AFI-0021 Doctor Old records check service done

--- src/main/java/com/team9/had/controller/ControllerForDoctor.java
+++ src/main/java/com/team9/had/controller/ControllerForDoctor.java
@@ -2,6 +2,7 @@ package com.team9.had.controller;
 
 import com.team9.had.exception.InternalServerError;
 import com.team9.had.model.doc.HrModelForDoc;
+import com.team9.had.model.doc.StartDateEndDateModel;
 import com.team9.had.service.doctor.ServiceForDoctor;
 import com.team9.had.utils.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,15 @@ public class ControllerForDoctor {
     public ResponseEntity<Serializable> getNewHealthRecords(@RequestParam String loginId, @RequestAttribute String role) throws InternalServerError {
         if(Constant.isAuthorised(role, Constant.DOCTOR)){
             Serializable obj = serviceForDoctor.getNewHealthRecords(loginId);
+            return new ResponseEntity<>(obj, HttpStatusCode.valueOf(Constant.HTTP_OK));
+        }
+        return new ResponseEntity<>(Constant.EMPTY, HttpStatusCode.valueOf(Constant.HTTP_UNAUTHORISED));
+    }
+
+    @PostMapping("/getOldHealthRecords")
+    public ResponseEntity<Serializable> getOldHealthRecords(@RequestParam String loginId, @RequestBody StartDateEndDateModel startDateEndDateModel, @RequestAttribute String role) throws Exception {
+        if(Constant.isAuthorised(role, Constant.DOCTOR)){
+            Serializable obj = serviceForDoctor.getOldHealthRecords(loginId, startDateEndDateModel);
             return new ResponseEntity<>(obj, HttpStatusCode.valueOf(Constant.HTTP_OK));
         }
         return new ResponseEntity<>(Constant.EMPTY, HttpStatusCode.valueOf(Constant.HTTP_UNAUTHORISED));
